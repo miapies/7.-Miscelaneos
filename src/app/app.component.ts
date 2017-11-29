a55f15f77144d23daa1d649095395cca210ad41e
Jimenez Jimenez <m.jimenez.jimenez@accenture.com>
2017-11-29 18:44:26 +0100

    Pause y Continue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,24 +13,30 @@ export class MyApp {
   rootPage: any;
 
   constructor(private _ajustes: AjustesProvider,
-    platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    private platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
 
       this._ajustes.cargar_storage()
-        .then((mensaje) => {
+        .then(() => {
           if (this._ajustes.ajustes.mostrar_tutorial) {
             this.rootPage = 'intro-page';
           } else {
             this.rootPage = HomePage;
           }
 
+          this.platform.pause.subscribe(() => {
+            console.log('La aplicación se detendrá');
+          });
+
+          this.platform.resume.subscribe(() => {
+            console.log('La aplicación va a continuar');
+          });
+
           // Okay, so the platform is ready and our plugins are available.
           // Here you can do any higher level native things you might need.
           statusBar.styleDefault();
           splashScreen.hide();
-        })
-        .catch((error) => console.log('Error al cargar: ' + JSON.stringify(error))
-        );
+        });
 
     });
   }
